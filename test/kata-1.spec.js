6edb5a0f40cd4865a542c8054c2df58a21868108
Jorge Caster <hola@jorge-castano.com>
2020-06-21 03:29:16 +0200

    updated test1

--- test/kata-1.spec.js
+++ test/kata-1.spec.js
@@ -2,17 +2,17 @@ import assert from 'assert';
 
 import kata1 from '../src/kata-1';
 
-describe('#kata-1', function () {
+describe('#kata-01', function () {
   it('should return false when nothing is sent', function () {
     assert.equal(kata1([]), false);
   });
 
   it('should return true when the array is [1, 1, 2, 2, 3, 5, 9, 9]', function () {
-    assert.equal(kata1([1, 1, 2, 2, 3, 5, 9, 9]), true);
+    assert.ok(kata1([1, 1, 2, 2, 3, 5, 9, 9]));
   });
 
   it('should return true when the array is [7, 7, 19]', function () {
-    assert.equal(kata1([7, 7, 19]), true);
+    assert.ok(kata1([7, 7, 19]));
   });
 
   it('should return false when the array is [95, 114]', function () {
